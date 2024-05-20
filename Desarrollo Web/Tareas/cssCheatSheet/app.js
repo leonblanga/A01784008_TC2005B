717e8a5b8622c5f73409739166e73cefd9563b08
Leon Blanga <a01784008@tec.mx>
2024-05-20 10:10:41 -0600

    Se modificó el archivo app.js

--- Desarrollo Web/Tareas/cssCheatSheet/app.js
+++ Desarrollo Web/Tareas/cssCheatSheet/app.js
@@ -7,14 +7,12 @@ const app = express();
 const port = 3001;
 
 app.use(express.json());
+app.use(express.static('public'));
 
 app.get("/", (req, res) => {
     const file = fs.readFileSync('public/html/css_cheat_sheet.html', 'utf8');
     res.status(200).send(file);
 });
-
-// Asegúrate de que el path sea correcto
-app.use(express.static('public'));
 
 app.listen(port, () => {
     console.log(`Servidor corriendo en http://localhost:${port}`);
